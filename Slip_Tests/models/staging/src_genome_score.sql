fdfb4febe88ae7040ec1c17633fdf9864aec628a
Thando Calana <110775561+ThandoCalana@users.noreply.github.com>
2025-06-20 18:20:32 +0200

    slip_test addition

---
+++ Slip_Tests/models/staging/src_genome_score.sql
@@ -0,0 +1,10 @@
+-- For genome_scores
+WITH raw_genome_scores AS (
+    SELECT * FROM MOVIELENS.RAW.RAW_GENOME_SCORES
+)
+
+SELECT
+    movieId AS movie_id,
+    tagId AS tag_id,
+    relevance
+FROM raw_genome_scores
